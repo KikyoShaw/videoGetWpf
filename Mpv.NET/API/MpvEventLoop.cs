1813f18e9e8e46da51652d38ee13bc5af5e97e5c
shaw <1939484341@qq.com>
2022-10-21 18:14:22 +0800

    新增mpv播放

---
+++ Mpv.NET/API/MpvEventLoop.cs
@@ -0,0 +1,121 @@
+﻿using Mpv.NET.API.Interop;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mpv.NET.API
+{
+	public class MpvEventLoop : IMpvEventLoop, IDisposable
+	{
+		public bool IsRunning { get => isRunning; private set => isRunning = value; }
+
+		public Action<MpvEvent> Callback { get; set; }
+
+		public IntPtr MpvHandle
+		{
+			get => mpvHandle;
+			private set
+			{
+				if (value == IntPtr.Zero)
+					throw new ArgumentException("Mpv handle is invalid.");
+
+				mpvHandle = value;
+			}
+		}
+
+		public IMpvFunctions Functions
+		{
+			get => functions;
+			set
+			{
+				Guard.AgainstNull(value);
+
+				functions = value;
+			}
+		}
+
+		private IntPtr mpvHandle;
+		private IMpvFunctions functions;
+
+		private Task eventLoopTask;
+
+		private bool disposed = false;
+		private volatile bool isRunning;
+
+		public MpvEventLoop(Action<MpvEvent> callback, IntPtr mpvHandle, IMpvFunctions functions)
+		{
+			Callback = callback;
+			MpvHandle = mpvHandle;
+			Functions = functions;
+		}
+
+		public void Start()
+		{
+			Guard.AgainstDisposed(disposed, nameof(MpvEventLoop));
+
+			DisposeEventLoopTask();
+
+			IsRunning = true;
+
+			eventLoopTask = new Task(EventLoopTaskHandler);
+			eventLoopTask.Start();
+		}
+
+		public void Stop()
+		{
+			Guard.AgainstDisposed(disposed, nameof(MpvEventLoop));
+
+			IsRunning = false;
+
+			if (Task.CurrentId == eventLoopTask.Id)
+			{
+				return;
+			}
+
+			// Wake up WaitEvent in the event loop thread
+			// so we can stop it.
+			Functions.Wakeup(mpvHandle);
+
+			eventLoopTask.Wait();
+		}
+
+		private void EventLoopTaskHandler()
+		{
+			while (IsRunning)
+			{
+				var eventPtr = Functions.WaitEvent(mpvHandle, Timeout.Infinite);
+				if (eventPtr != IntPtr.Zero)
+				{
+					var @event = MpvMarshal.PtrToStructure<MpvEvent>(eventPtr);
+					if (@event.ID != MpvEventID.None)
+						Callback?.Invoke(@event);
+				}
+			}
+		}
+
+		private void DisposeEventLoopTask()
+		{
+			eventLoopTask?.Dispose();
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				if (!disposed)
+				{
+					Stop();
+
+					DisposeEventLoopTask();
+				}
+
+				disposed = true;
+			}
+		}
+	}
+}
--- videoGet/MainWindow.xaml.cs
+++ videoGet/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Mpv.NET.Player;
 
 namespace videoGet
 {
@@ -20,9 +22,16 @@ namespace videoGet
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MpvPlayer _player;
+
         public MainWindow()
         {
             InitializeComponent();
+            _player = new MpvPlayer(PlayerHost.Handle)
+            {
+                Loop = true,
+                Volume = 50
+            };
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
@@ -32,35 +41,85 @@ namespace videoGet
             if(string.IsNullOrEmpty(text))
                 return;
 
+            _player.Load(text);
+            _player.Resume();
+        }
+
+        private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = UrlTextBox.Text;
+            if (string.IsNullOrEmpty(text))
+                return;
+            //抖音
+            var vm = DouYinGet.Instance;
+            vm.InitUrl(text);
+            var name = vm.GetTitle();
+            this.VideoName.Text = name;
+            var url = vm.GetVideoUrl();
+            MpvPlay(url);
+        }
+
+        private void ButtonBase3_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = UrlTextBox.Text;
+            if (string.IsNullOrEmpty(text))
+                return;
             //微视
             var vm = WeiShiGet.Instance;
             vm.InitUrl(text);
+            var name = vm.GetTitle();
+            this.VideoName.Text = name;
             var url = vm.GetMp4();
+            MpvPlay(url);
+        }
 
-            //抖音
-            //var vm = DouYinGet.Instance;
-            //vm.InitUrl(text);
-            //var url = vm.GetVideoUrl();
-
-            //快手
-            //var vm = KuaiShouGet.Instance;
-            //vm.InitUrl(text);
-            //var url = vm.GetMp4();
-
+        private void ButtonBase4_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = UrlTextBox.Text;
+            if (string.IsNullOrEmpty(text))
+                return;
             //西瓜视频
-            //var vm = XiGuaGet.Instance;
-            //vm.InitUrl(text);
-            //var url = vm.Get1080Mp4();
+            var vm = XiGuaGet.Instance;
+            vm.InitUrl(text);
+            var name = vm.GetTitle();
+            this.VideoName.Text = name;
+            var url = vm.Get1080Mp4();
+            MpvPlay(url);
+        }
 
+        private void ButtonBase5_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = UrlTextBox.Text;
+            if (string.IsNullOrEmpty(text))
+                return;
+            //微视
+            var vm = WeiShiGet.Instance;
+            vm.InitUrl(text);
+            var name = vm.GetTitle();
+            this.VideoName.Text = name;
+            var url = vm.GetMp4();
+            MpvPlay(url);
+        }
+
+        private void MpvPlay(string url)
+        {
             if (!string.IsNullOrEmpty(url))
             {
-                this.Player.SourceStr = url;
+                if (_player.IsPlaying)
+                    _player.Stop();
+                _player.Load(url);
+                _player.Resume();
             }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            this.Player.SourceStr = @"http://v.weishi.qq.com/v.weishi.qq.com/gzc_8542_1047_0bc3w4buyaadeiagu3zzwzrrjnyejs3qgtca.f70.mp4?dis_k=c9911306a3279b023cc404b14128956c&dis_t=1666339506&fromtag=0&pver=8.79.1";
+            //this.Player.SourceStr = @"http://v.weishi.qq.com/v.weishi.qq.com/gzc_8542_1047_0bc3w4buyaadeiagu3zzwzrrjnyejs3qgtca.f70.mp4?dis_k=c9911306a3279b023cc404b14128956c&dis_t=1666339506&fromtag=0&pver=8.79.1";
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            _player.Dispose();
         }
     }
 }
